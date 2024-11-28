288e0a6ee9445ed42e5c2cbfbcf9d4da3f25f554
kam1lov <amirbekk@gmail.com>
2024-11-28 07:12:50 +0500

    fix: add transition

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,32 @@ import Navbar from './components/navbar.vue';
 
 <template>
      <Navbar/>
-  <RouterView />
+     <Transition
+      enter-active-class="fade-enter-active"
+      enter-from-class="fade-enter-from"
+      enter-to-class="fade-enter-to"
+      leave-active-class="fade-leave-active"
+      leave-from-class="fade-leave-from"
+      leave-to-class="fade-leave-to"
+      >
+    
+      <RouterView />
+  </Transition>
 </template>
 
 <style scoped>
-
+ .fade-enter-active, .fade-leave-active {
+ transition: opacity 0.5s ease, transform 0.5s ease;
+ }
+ 
+ .fade-enter-from, .fade-leave-to {
+ opacity: 0;
+ transform: translateX(-100%);
+ }
+ 
+ .fade-enter-to, .fade-leave-from {
+ opacity: 1;
+ transform: translateX(0);
+ }
+ 
 </style>
--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -5,8 +5,8 @@
                     <h1>Amirbek <span></span></h1>
 
                 <ul>
-                    <RouterLink to="/">Home</RouterLink>
-                    <RouterLink to="/blogs">Blogs</RouterLink>
+                    <RouterLink to="/" class="nav-link">Home</RouterLink>
+                    <RouterLink to="/blogs" class="nav-link">Blogs</RouterLink>
                     <li><img src="../../public/navbar/Instagram.svg"><a href="#">Instagram</a></li>
                     <li><img src="../../public/navbar/Discoard.svg"><a href="#">Discord</a></li>
                     <li><img src="../../public/navbar/Github.svg"><a href="#">Github</a></li>
@@ -18,4 +18,25 @@
 
 <script setup></script>
 
-<style scoped></style>
+<style scoped>
+
+
+.nav-link {
+  text-decoration: none;
+  color: black; 
+  font-weight: normal;
+}
+
+
+.router-link-active {
+  color: #12F7D6;
+  font-weight: bold;
+}
+
+
+.router-link-exact-active {
+  color: #12F7D6;
+  font-weight: bold;
+}
+
+</style>
